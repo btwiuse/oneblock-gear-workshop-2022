9e9bc1bc97d0b7aa21f3a10d43638f4679cc2d55
navigaid <navigaid@gmail.com>
2023-01-06 11:28:56 +0800

    rm rust code

--- hello-gear/src/lib.rs
+++
@@ -1,7 +0,0 @@
-#![no_std]
-
-#[no_mangle]
-extern "C" fn handle() {
-  let _ = gstd::msg::load_bytes(); // read input message and do nothing
-  gstd::msg::reply_bytes(gstd::String::from("Hello world!"), 0);
-}
